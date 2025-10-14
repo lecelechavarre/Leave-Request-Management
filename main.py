81fef6a8a0c6c0b5508a0986c1bfabcf53a1c4c9
Lecel Ann Harvey Echavarre <161615845+lecelechavarre@users.noreply.github.com>
2025-10-14 10:55:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PyQt6.QtWidgets import QApplication
 from app import AppController
 import sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     controller = AppController()
     controller.show_login()
