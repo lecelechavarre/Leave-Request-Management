cb9f011783888c3bc05e8e7abcb5aa814cc22e53
Lecel Ann Harvey Echavarre <161615845+lecelechavarre@users.noreply.github.com>
2025-10-14 10:56:28 +0800

    Adding functions

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from views.login_window import LoginWindow
 from models.user_model import UserModel
+from views.login_window import LoginWindow
+from views.dashboard_window import DashboardWindow
 
 class AppController:
     def __init__(self):
+        self.user = None
         self.user_model = UserModel()
-        self.login_window = None
+        self.login_window = LoginWindow(self)
+        self.dashboard = None
 
     def show_login(self):
-        self.login_window = LoginWindow(self)
         self.login_window.show()
 
     def login_success(self, user):
-        print(f"Login success: {user['username']} ({user['role']})")
+        self.user = user
+        self.dashboard = DashboardWindow(self, user)
+        self.login_window.close()
+        self.dashboard.show()
+
+    def logout(self):
+        if self.dashboard:
+            self.dashboard.close()
+        self.user = None
+        # create a fresh login window
+        self.login_window = LoginWindow(self)
+        self.login_window.show()
