ab40e1a90d5b2abb15410a225931d43bdcbf5142
Lecel Ann Harvey Echavarre <161615845+lecelechavarre@users.noreply.github.com>
2025-10-15 14:26:35 +0800

    Add functions

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from PyQt6.QtWidgets import QApplication
 from app import AppController
-import sys
+import sys, os
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    # load QSS
+    qss_path = os.path.join(os.path.dirname(__file__), 'resources', 'styles.qss')
+    if os.path.exists(qss_path):
+        with open(qss_path, 'r', encoding='utf-8') as f:
+            app.setStyleSheet(f.read())
     controller = AppController()
     controller.show_login()
     sys.exit(app.exec())
